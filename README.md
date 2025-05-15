0d9beb4d8cb99accb132ddd43327555abc0fd0b9
Chi-Kit Pao <Chi-KitPao@gmx.de>
2025-05-15 21:53:28 +0200

    Added SudokuSolver

--- README.md
+++ README.md
@@ -11,29 +11,37 @@ You need to install Python, PuLP and Matplotlib to run my python scripts.
 
 File list
 ------------
-- **CircleInPolygon.py**: Calculate circle with maximum area within a specified polygon.
-- **CircleInPolygon_Screenshot.png**: Screenshot of program CircleInPolygon.py.
-- **PolycubePuzzle.py**: Solve polycube puzzle either with identical pieces or different pieces.
-- **PolycubePuzzleMultiple.png**: Solution plot for program PolycubePuzzle.py with option "--multiple".
-- **PolycubePuzzleSingle.png**: Solution plot for program PolycubePuzzle.py with option "--single".
-- **SimpleClassifier.py**: Calculate classifier for data with two variables.
-- **SimpleClassifier_Screenshot1.png**: Screenshot of program SimpleClassifier.py when clear classification is possible.
-- **SimpleClassifier_Screenshot2.png**: Screenshot of program SimpleClassifier.py when clear classification is not possible.
+- **CircleInPolygon**
+    - **CircleInPolygon.py**: Calculate circle with maximum area within a specified polygon.
+    - **CircleInPolygon_Screenshot.png**: Screenshot of program CircleInPolygon.py.
+- **PolycubePuzzle**
+    - **PolycubePuzzle.py**: Solve polycube puzzle either with identical pieces or different pieces.
+    - **PolycubePuzzleMultiple.png**: Solution plot for program PolycubePuzzle.py with option "--multiple".
+    - **PolycubePuzzleSingle.png**: Solution plot for program PolycubePuzzle.py with option "--single".
+- **SimpleClassifier**
+    - **SimpleClassifier.py**: Calculate classifier for data with two variables.
+    - **SimpleClassifier_Screenshot1.png**: Screenshot of program SimpleClassifier.py when clear classification is possible.
+    - **SimpleClassifier_Screenshot2.png**: Screenshot of program SimpleClassifier.py when clear classification is not possible.
+- **SudokuSolver**
+    - **SudokuSolver.py**: Solve Sudoku puzzle.
+    - **sudoku.txt**: Example sudoku input file.
+    - **Sudoku.png**: Screenshot of program SodokuSolver.py.
 
 Usage
 ------------
-- CircleInPolygon.py: Put coordinates of the polygon into the list 'points' in function 'main' and then run the program. Might not work for concave polygons at the moment.
-- PolycubePuzzle.py: It will just print the solution of the Polycube Puzzle.
+- CircleInPolygon/CircleInPolygon.py: Put coordinates of the polygon into the list 'points' in function 'main' and then run the program. Might not work for concave polygons at the moment.
+- PolycubePuzzle/PolycubePuzzle.py: It will just print the solution of the Polycube Puzzle.
     * 1: Solving puzzle with identical pieces
         * 1.1: Change the puzzle of variable "raw_polycube" in function "main", if necessary.
         * 1.2: Run the program either without option or with option "--single".
     * 2: Solving puzzle with different pieces
         * 2.1: Change the puzzle of variable "raw_polycubes" in function "main", if necessary.
         * 2.2: Run the program with option "--multiple".
-- SimpleClassifier.py: 
+- SimpleClassifier/SimpleClassifier.py: 
     * Step 1: Put data into the list 'data' in function 'main'.
     * Step 2: Change 'x_label', 'y_label', and 'forced_buffer' to proper values.
     * Step 3: Run the program.
+- SudokuSolver/SudokuSolver.py: Either change the variable "sudoku" or specify the input file name as program parameter. Put 0 for unknown numbers.
 
 
 License & Copyright
---
+++ SudokuSolver/SudokuSolver.py
@@ -0,0 +1,131 @@
+"""  
+    Solves Sudoku puzzle.
+
+    Author: Chi-Kit Pao
+    REMARK: Requires library PuLP and Matplotlib to run this program.
+    USAGE:  Either change the variable "sudoku" or specify the input 
+            file name as program parameter. Put 0 for unknown numbers.
+"""
+
+import itertools
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator, NullLocator
+import numpy as np
+import sys
+import pulp as plp
+
+
+sudoku = ["030000000",
+          "000195000",
+          "008000060",
+          "800060000",
+          "400800001",
+          "000020000",
+          "060000280",
+          "000419005",
+          "000000070"]
+
+class Sudoku:
+    def __init__(self, lines):
+        self.lines = []
+        for line in lines:
+            self.lines.append(list(map(lambda c: ord(c) - ord('0'), line)))
+
+    def solve(self):
+        # Use linear programming to find a possible solution of a given Sudoku puzzle.
+        # The implementation here is loosely based on the consideration from the website
+        # https://www.coin-or.org/PuLP/CaseStudies/a_sudoku_problem.html
+
+        # Objective: Not really needed. Can use default sense (=const.LpMinimize).
+        problem = plp.LpProblem(f'FindSolution')
+
+        # Objective function not really needed.
+
+        # Decision variables: row, column and value
+        lp_var_cells = []
+        for i in range(0, 9):
+            row_list = []
+            for j in range(0, 9):
+                cell_list = [plp.LpVariable(f'x{i}_{j}_{v}', 0, 1, cat=plp.const.LpBinary) for v in range(1, 10)]
+                row_list.append(cell_list)
+            lp_var_cells.append(row_list)
+
+        for i in range(0, 9):
+            for j in range(0, 9):
+                input_value = self.lines[i][j]
+                if input_value != 0:
+                    # Add constraint: Set lp variables of known values
+                    for v in range(1, 10):
+                        problem += lp_var_cells[i][j][v-1] == (1 if v == input_value else 0)
+
+                # Add constraint: Only one variable of a cell is one and others are zero.
+                problem += plp.lpSum([lp_var_cells[i][j][v-1] for v in range(1, 10)]) == 1
+
+        # Add further constraints
+        # Row Constraint: Every number only occurs once in a row.
+        for i in range(0, 9):
+            for v in range(1, 10):
+                problem += plp.lpSum([lp_var_cells[i][j][v-1] for j in range(0, 9)]) == 1
+        # Column Constraint: Every number only occurs once in a column.
+        for j in range(0, 9):
+            for v in range(1, 10):
+                problem += plp.lpSum([lp_var_cells[i][j][v-1] for i in range(0, 9)]) == 1
+        # Constraint for subsquare: Every number only occurs once in a sub-square.
+        for i, j in itertools.product((0, 3, 6), repeat=2):
+            for v in range(1, 10):
+                l = [lp_var_cells[i+di][j+dj][v-1] for di, dj in itertools.product(list(range(3)), repeat=2)]
+                problem += plp.lpSum(l) == 1
+
+        status = problem.solve(plp.PULP_CBC_CMD(msg=False))
+        if status != 1:
+            raise RuntimeError(f"No solution found for Sudoku! Pulp problem status: {status}!")
+
+        self.__write_back(lp_var_cells)
+
+    def __write_back(self, lp_var_cells):
+        for i, j in itertools.product(range(0, 9), repeat=2):
+            v = len(list(itertools.takewhile(lambda x: x.value() == 0, lp_var_cells[i][j]))) + 1
+            if self.lines[i][j] == 0:
+                assert 1 <= v <= 9, f"{i=}, {j=}, {v=}"
+                self.lines[i][j] = v
+            else:
+                assert self.lines[i][j] == v
+                pass
+
+def plot_sudoku(s):
+    fig, ax = plt.subplots()
+    max_num = 9
+    for i, j in itertools.product(list(range(0,max_num)), repeat=2):
+        ax.text(i+0.5,j+0.5,str(s.lines[8-j][i]), ha='center', va='center')
+
+    ax.axis([0, max_num, 0, max_num])
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_visible(False)
+        axis.set_ticks(np.arange(max_num) + 0.5)
+        axis.set_ticklabels("")
+
+    for i in range(max_num):
+        ax.axhline(y=(i))
+        ax.axvline(x=(i))
+
+    title = "Sudoku"
+    fig.suptitle(title)
+    fig.canvas.manager.set_window_title(title)
+    plt.show()
+
+def main():
+    global sudoku
+    if len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as f:
+            sudoku = list(map(lambda line: line.replace('\n', ''), f.readlines()))
+    # else keep internal variable the same
+    
+    s = Sudoku(sudoku)
+    s.solve()
+    for line in s.lines:
+        print(line)
+
+    plot_sudoku(s)
+
+if __name__ == '__main__':
+    main()
